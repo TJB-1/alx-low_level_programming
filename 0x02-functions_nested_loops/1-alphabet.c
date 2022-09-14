892a16148ecfc342f64a4e07f1c3de0c76ecc8d4
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-14 10:10:40 -0700

    Task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main-h"
+
+/**
+ * main - Entry point 
+ * Description - Print the alpahbet in lowercase
+ * Return - 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = a, i <= 'z', i++);
+	{
+		_putchar(i);
+	}
+	return ('/n');
+}
