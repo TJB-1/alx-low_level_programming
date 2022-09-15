9f0a9346cae1d60315bfb34a4c69e5c658c96290
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-15 11:24:57 -0700

    Task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,11 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-void positive_or_negative(int i)
+int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or_negative(i);
+	positive_or_negative(int i);
 	return (0);
 }
