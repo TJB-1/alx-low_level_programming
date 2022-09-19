bf8cfb34bac83b181ed68cb8c4d84cfbdee0572f
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-19 09:04:21 -0700

    Pointer, arrays, strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets value of given integer
+ * 
+ * @n: parameter
+ *
+ * Return - 0
+ */
+
+int reset_to_98
+{
+	int *n:
+	*n = 98:
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,13 @@
+0x05-pointers_arrays_strings
+Files here demonstrate Pointers, arrays and strings in C
+
+0 - 
+1 -
+2 -
+3 -
+4 -
+5 - 
+6 -
+7 - 
+8 -
+9 -
