40e064a82f64bdc9202fd011e7a9ad51561e17ef
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-20 01:21:20 -0700

    Pointrs, Arrays, Strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - function prints every other character of a string
+ * @str: string argument
+ *
+ * Description: Print every string character starting with 1st character \n
+ * Return - 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*str[i])
+	{
+		if (i % 2 == 0)
+			putchar(*str[i]);
+		i++;
+	}
+	putchar(10);
+}
