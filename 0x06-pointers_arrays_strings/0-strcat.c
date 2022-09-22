29f12b7d7bd135f4c4e499ee743793513ce17ab3
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-22 05:39:28 -0700

    Pointers_Arrays_Strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[j]  != '\n'; &&src[i] != '\n')
+	while (dest[j]  != '\n' && src[i] != '\n')
 	{
 		dest[i] = src[i];
 		i++;
