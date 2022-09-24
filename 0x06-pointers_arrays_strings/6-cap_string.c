37473f01014633ac3b3e48e67af786c741584d43
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-23 23:01:46 -0700

    PAS 2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,37 +9,36 @@
 
 char *cap_string(char *str)
 {
-		int i = 0, j, k;
-			char delimiters[] = " \t\n,;.!?\"(){}";
+	int i = 0, j, k;
+	char delimiters[] = " \t\n,;.!?\"(){}";
 
-				for (; str[i]; i++)
-						{
-									if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
-												{
-																str[i] -= 32;
-																		}
+	for (; str[i]; i++)
+	{
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= 32;
+		}
+		j = 0;
 
-											j = 0;
-
-													for (; delimiters[j]; j++)
-																{
-																				if (str[i] == delimiters[j])
-																								{
-																													i++;
-
-																																	for (k = 0; delimiters[k]; k++)
-																																						{
-																																												if (str[i] == delimiters[k])
-																																																		{
-																																																									i++;
-																																																														}
-																																																}
-																																					if (str[i] >= 'a' && str[i] <= 'z')
-																																										{
-																																																str[i] -= 32;
-																																																				}
-																																								}
-																						}
-														}
-					return (str);
+		for (; delimiters[j]; j++)
+		{
+			if (str[i] == delimiters[j])
+			{
+				i++;
+	
+				for (k = 0; delimiters[k]; k++)
+				{
+					if (str[i] == delimiters[k])
+					{
+						i++;
+					}
+				}
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+		}
+	}
+}
+return (str);
 }
