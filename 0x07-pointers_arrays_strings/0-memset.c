cdc1c6a73b93d4d8fb244830a7b28c29471b9573
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-26 09:11:57 -0700

    More PAS 3

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "mai.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: starting address of memory to be filled
+ *  (Pointer-to-pointer/double pointer- Holds address to another pointer)
+ * @b: value to be filled
+ *  (Normal pointer that holds the address of an integer)
+ * @n: Number of bytes to be filled starting from se
+ *  (Variable containing address)
+ * Return: Pointer to memory are s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int z = 0;
+
+	/*Assigning address of pointer to b to double pointer *s */
+	for ( ; z < n; z++)
+	{
+		b = &*s;
+	}
+
+}
