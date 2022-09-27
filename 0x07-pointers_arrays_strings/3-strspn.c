ae77caf9e7f035891fdf4047685093c6385f572f
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-27 08:36:56 -0700

    More PAS

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: string to check
+ * @accept:string to check against
+ * Return: Number of bytes of  s in accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x]; x++)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+			if (s[x] == accept[y])
+				break;
+		}
+		if (!accept[y]
+			break;
+	}
+	return (x);
+}
