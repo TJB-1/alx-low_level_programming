659f570c38b4ee632271a12cfd36ad78d0f77e80
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-29 11:10:37 -0700

    Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,11 +9,11 @@
 void _print_rev_recursion(char *s)
 {
 	/* check for when character is not null*/
-	if (*s != '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
 	s++;
 	_print_rev_recursion(s);
-	_putchar(--s);
+	_putchar(*--s);
 }
