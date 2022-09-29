cae2a851ccc16553594b868019b9550f098b853a
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-29 11:35:56 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	_strlen_recursion(s);
+	return (1 + _strlen_recursion(s));
+}
