813215abe132ed3059008a582c70f735e4a49b5f
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-09-28 22:39:28 -0700

    Recursio

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by \n
+ * @s: string to be printed
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{	
+	if (*s == '\0')
+	{
+		_putchar('\n');
+			return (0);
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}}
