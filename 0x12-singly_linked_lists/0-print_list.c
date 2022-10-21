8dba8ef150fa0cc18fae6b334335c26d1ae9837c
Elikem .A. Bansah <elikembansah27@gmail.com>
2022-10-21 05:35:51 -0700

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "list.h"
+
+/**
+ * print_list -  prints all the elements of a list_t list.
+ * @h: a pointer to the first element of list
+ * Return - Number of nodes or elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t elements = 0;
+
+	if (!h)
+	{
+		return (elements);
+	}
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,7 @@
+0x12-singly_linked_lists
+
+Reads:
+1. https://alx-intranet.hbtn.io/concepts/120
+2. https://alx-intranet.hbtn.io/rltoken/joxg32-tt4lUh8Afgst8tA
+3. https://alx-intranet.hbtn.io/rltoken/USaZbNdfcuIFII-K2YPsKQ
+4. https://alx-intranet.hbtn.io/rltoken/epKUCIcoA6XaN1T3Vtr_9w
