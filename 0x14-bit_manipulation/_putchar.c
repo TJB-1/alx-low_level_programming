f8b6e04387b45e6ff7aac972f5199dcf8899e698
Elikem .A. Bansah <53125150+TJB-1@users.noreply.github.com>
2022-10-28 06:24:37 +0000

    Update _putchar.c

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include <unstdio.h>
+
+/**
+ * _putchar - prints character to standard output
+ * @s: characterr
+ * Return: 1 if sucessful and -1 for faiure
+ */
+
+int _putchar(char s)
+{
+	return (write(1, &c, 1));
+}
